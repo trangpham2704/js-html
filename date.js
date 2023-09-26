b9640216a40e7ac92296d7491a9efd215e78c535
trangpham2704 <101716551+trangpham2704@users.noreply.github.com>
2023-09-26 20:43:24 +0700

    Create date.js

---
+++ date.js
@@ -0,0 +1,25 @@
+class MyDate {
+    constructor(day, month, year) {
+          this.day = day;
+          this.month = month;
+          this.year = year;
+    }
+    getDay() {
+        return this.day;
+    }
+    getMonth() {
+        return this.month;
+    }
+    getYear() {
+        return this.year;
+    }
+    setDay(day) {
+        this.day  = day;
+    }
+    setMonth(month) {
+        this.month  = month;
+    }
+    setYear(year) {
+        this.year  = year;
+    }
+  }
